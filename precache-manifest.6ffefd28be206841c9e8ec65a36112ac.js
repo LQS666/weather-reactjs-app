8749824c026f188c5aa3f58e2a14491eaa72c23f
Paweł Turkot <p.turkot123@gmail.com>
2019-03-14 20:31:54 +0100

    Updates

---
+++ precache-manifest.6ffefd28be206841c9e8ec65a36112ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b99b5a2533b0407b7eaff27f1a9955c",
+    "url": "/weather-reactjs-app/static/media/logo.0b99b5a2.png"
+  },
+  {
+    "revision": "a523a4e5725b2b1814cc7d55d262e916",
+    "url": "/weather-reactjs-app/static/media/background.a523a4e5.jpg"
+  },
+  {
+    "revision": "ad7412950d20bb45d60b",
+    "url": "/weather-reactjs-app/static/js/runtime~main.ad741295.js"
+  },
+  {
+    "revision": "5074858dee78acb79f76",
+    "url": "/weather-reactjs-app/static/js/main.5074858d.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/weather-reactjs-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5074858dee78acb79f76",
+    "url": "/weather-reactjs-app/static/css/main.589d5dd3.chunk.css"
+  },
+  {
+    "revision": "350d6c57fe2474ebeb4be22685a0d1c7",
+    "url": "/weather-reactjs-app/index.html"
+  }
+];
